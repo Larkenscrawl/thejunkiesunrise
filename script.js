fb7ded6f4039330475e66c3942869af512f87c6e
Larkenscrawl <hughgodolphin@gmail.com>
2024-06-09 03:34:45 +1000

    New files
    New files

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleText(element) {
+    const textOptions = {
+        "She saw her ghost self put a pill on her ghost tongue, walk to the ghost trick and stick it in his mouth. She saw the ghost trick swallow it and pass out. Her ghost self then rummaged through the ghost trick’s coat pocket, grabbed his wallet, and bailed.": "She saw her ghost self having sex with the ghost trick. Afterwards, as the ghost trick was sleeping, her ghost self sat down on the floor, took a loaded syringe from her purse, shot up and nodded off. The ghost trick woke up a while later, took her purse, and took off.",
+        "She saw her ghost self having sex with the ghost trick. Afterwards, as the ghost trick was sleeping, her ghost self sat down on the floor, took a loaded syringe from her purse, shot up and nodded off. The ghost trick woke up a while later, took her purse, and took off.": "She saw ghost self giving the ghost trick a few oxys. She noticed that her ghost self put a benzo in there by accident. The ghost trick crushed the pills and smoked them. He lay down in bed, and after a while, he stopped breathing. Her ghost self tried to shake him out of it but couldn’t. She said “oh shit” and bolted.",
+        "She saw ghost self giving the ghost trick a few oxys. She noticed that her ghost self put a benzo in there by accident. The ghost trick crushed the pills and smoked them. He lay down in bed, and after a while, he stopped breathing. Her ghost self tried to shake him out of it but couldn’t. She said “oh shit” and bolted.": "She saw her ghost self put a pill on her ghost tongue, walk to the ghost trick and stick it in his mouth. She saw the ghost trick swallow it and pass out. Her ghost self then rummaged through the ghost trick’s coat pocket, grabbed his wallet, and bailed.",
+        "“Fuck off,” she said.": "“Make it a hundred” she said.",
+        "“Make it a hundred” she said.": "“Fuck you even talking to me for? I’m gonna call my dad!”",
+        "“Fuck you even talking to me for? I’m gonna call my dad!”": "“Fuck off,” she said.",
+        "“Just give me the black tar,” she said.": "“Ok, but gimme a fresh one” she said.",
+        "“Ok, but gimme a fresh one” she said.": "“Fine, but can I borrow some cash? I need to get a tooth fixed. Tricks are complainin’,” She said.",
+        "“Fine, but can I borrow some cash? I need to get a tooth fixed. Tricks are complainin’,” She said.": "“Just give me the black tar,” she said.",
+        "“I can’t, Haley… I’ve tried…”": "“I can’t, Haley…”",
+        "“I can’t, Haley…”": "“I can’t Haley, it’s too much,” Maggie said.",
+        "“I can’t Haley, it’s too much,” Maggie said.": "“I can’t, Haley… I’ve tried…”",
+        "shooting up her arm, closing her eyes, and dying, thick spit trickling down her hung head.": "shooting up her neck, rolling her eyes until it was all white like a statue, and dying.",
+        "shooting up her neck, rolling her eyes until it was all white like a statue, and dying.": "taking off her heels and shooting up between her toes and slumping over, dead, arms outstretched like a sleeping child, a little blood from the head from slamming on the ground.",
+        "taking off her heels and shooting up between her toes and slumping over, dead, arms outstretched like a sleeping child, a little blood from the head from slamming on the ground.": "shooting up the artery and flopping around in agony like a fish on a boat, screaming so hard her voice cracked and vanished, then dying with her hip thrust in the air like a yoga pose.",
+        "shooting up the artery and flopping around in agony like a fish on a boat, screaming so hard her voice cracked and vanished, then dying with her hip thrust in the air like a yoga pose.": "dying in the middle of the bandstand.",
+        "dying in the middle of the bandstand.": "dying at the entrance of the bandstand.",
+        "dying at the entrance of the bandstand.": "dying facing out.",
+        "dying facing out.": "dying facing in.",
+        "dying facing in.": "dying facing up.",
+        "dying facing up.": "dying facing down.",
+        "dying facing down.": "dying.",
+        "dying.": "dying."
+    };
+
+    element.innerHTML = textOptions[element.innerHTML] || element.innerHTML;
+}
